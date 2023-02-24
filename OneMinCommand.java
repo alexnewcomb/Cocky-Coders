e371e0a4c072fe2c2d9c64fd2a5c2138fd73983b
Jose Armando Gonzalez Jr <gjoe344@gmail.com>
2023-02-24 00:28:56 -0500

    first commit - Jose - base code

---
+++ OneMinCommand.java
@@ -0,0 +1,14 @@
+
+public class OneMinCommand implements Command {
+        private StopWatch watch;
+
+        public OneMinCommand(StopWatch watch) {
+                this.watch = watch;
+        }
+
+        @Override 
+        public void execute() {
+                watch.one();
+        }
+
+}
