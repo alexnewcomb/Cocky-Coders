690463e965d2f87e86c0586b5d858a15379e3c92
Zoe McCuen <zoelenore@gmail.com>
2023-02-26 21:50:03 -0500

    finishing java doc

--- CockyCoders/InputHandler.java
+++ CockyCoders/InputHandler.java
@@ -6,8 +6,15 @@ package CockyCoders;
 import java.util.HashMap;
 
 public class InputHandler {
+    /**
+     * calls to other classes to start a count down based on a number input by the user 
+     */
     private HashMap<String, Command> commands;
 
+    /**
+     * uses a HashMap to call to other classes to output the countdown based on user input of 1,5,30, or q to quit
+     * @param watch
+     */
     public InputHandler(StopWatch watch) {
         commands = new HashMap<String, Command>();
         commands.put("1", new OneMinCommand(watch));
@@ -15,6 +22,11 @@ public class InputHandler {
         commands.put("30", new ThirtySecCommand(watch));
     }
 
+    /**
+     * creates parameters for the countdown to be executed based on user input
+     * @param key
+     * @return countdowns 
+     */
     public boolean inputEntered(String key) {
         Command command = commands.get(key);
         if (command != null) {
